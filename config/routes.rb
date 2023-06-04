708a731c0eab34ab7f30fe2156169f0dd32e2b17
Ruiyot Abby <ruiyotabby@gmail.com>
2023-06-04 18:28:45 +0300

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'static_pages/home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
